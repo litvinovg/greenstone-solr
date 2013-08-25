ddea8d331c53f8efd426d8a87dcc967cf3ab7e4d
davidb <davidb@a4f36ff5-3332-0410-bbfd-b1ff9fae65ff>
2013-08-25 23:47:57 +0000

    Added if-statement test to make sure sort_field exists before it is used.  Using the search box on the 'about' page is one place where this can occur, for example.
    git-svn-id: http://svn.greenstone.org/gs3-extensions/solr/trunk/src@28125 a4f36ff5-3332-0410-bbfd-b1ff9fae65ff

--- src/java/org/greenstone/gsdl3/util/SolrQueryWrapper.java
+++ src/java/org/greenstone/gsdl3/util/SolrQueryWrapper.java
@@ -53,7 +53,7 @@ public class SolrQueryWrapper extends SharedSoleneQuery
 
 	static Logger logger = Logger.getLogger(org.greenstone.gsdl3.util.SolrQueryWrapper.class.getName());
 	protected int max_docs = 100;
-  protected String sort_order = SORT_DESCENDING;
+	protected String sort_order = SORT_DESCENDING;
 	protected ArrayList<String> _facets = new ArrayList<String>();
 	protected ArrayList<String> _facetQueries = new ArrayList<String>();
 	SolrServer solr_core = null;
@@ -200,7 +200,11 @@ public class SolrQueryWrapper extends SharedSoleneQuery
 		ModifiableSolrParams solrParams = new ModifiableSolrParams();
 		solrParams.set("q", query_string);
 		// sort param, like "score desc" or "byORG asc"
-		solrParams.set("sort", this.sort_field+" "+this.sort_order);
+
+		if (this.sort_field != null) {
+		    solrParams.set("sort", this.sort_field+" "+this.sort_order);
+		}
+
 		// which result to start from
 		solrParams.set("start", start_results);
 		// how many results per "page"
