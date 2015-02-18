5e7eeffe01e5a013ec27cc856988baafce21580b
ak19 <ak19@a4f36ff5-3332-0410-bbfd-b1ff9fae65ff>
2015-02-18 03:35:18 +0000

    Bringing ext\solr's gs3-setup.bat up to speed with recent changse ing solr's gs3-setup.sh, now that solr's changed to use the GS3 tomcat server instead of solr's included jetty. gs3-setup.bat now reads tomcat.server and tomcat.port from build.properties to set SOLR_HOST and SOLR_PORT
    git-svn-id: http://svn.greenstone.org/gs3-extensions/solr/trunk/src@29749 a4f36ff5-3332-0410-bbfd-b1ff9fae65ff

--- gs3-setup.bat
+++ gs3-setup.bat
@@ -3,7 +3,27 @@ pushd "%CD%"
 CD /D "%~dp0"
 
 set extdesc=the Solr Extension
+if "%GSDL3SRCHOME%" == "" echo Run the toplevel gs3-setup.bat before calling this script& goto End
 
+rem IF USING TOMCAT SERVER FOR SOLR
+:: If using tomcat, read the tomcat host and port from the toplevel GS3 build.properties
+:: http://en.kioskea.net/faq/1757-how-to-read-a-file-line-by-line
+:: The following reads build.properties line by line, tokenizing it around the = sign, then
+:: sets the SOLR_PORT and SOLR_HOST to the values of tomcat.port and tomcat.host properties
+
+set SOLR_PORT=8983
+set SOLR_HOST=localhost
+:: Loosely based on 
+:: http://stackoverflow.com/questions/7708681/how-to-read-from-a-properties-file-using-batch-script 
+FOR /F "tokens=1,2 delims==" %%G IN (%GSDL3SRCHOME%\build.properties) DO ( 
+	if "%%G"=="tomcat.server" set SOLR_HOST=%%H
+	if "%%G"=="tomcat.port" set SOLR_PORT=%%H
+)
+echo Tomcat host: %SOLR_HOST%
+echo Tomcat port: %SOLR_PORT%
+
+
+rem IF USING JETTY SERVER FOR SOLR
 :: The port Jetty runs on:
 set SOLR_JETTY_PORT=8983
 
@@ -62,7 +82,8 @@ echo +Your environment is now setup for %extdesc%
 :: Back to delayed expansion to avoid problems with environment
 :: variables with brackets in them, such as "Program Files (x86)"
 
-echo ++Solr/Jetty server will run on port %SOLR_JETTY_PORT% (+ port %JETTY_STOP_PORT% for shutdown command)"
+::echo ++Solr/Jetty server will run on port %SOLR_JETTY_PORT% (+ port %JETTY_STOP_PORT% for shutdown command)
+echo ++Solr will run off the tomcat server on port %SOLR_PORT%
 echo --These port values can be changed by editing:
 echo --  %0
 
